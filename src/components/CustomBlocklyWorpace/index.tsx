7e459098d7363b28cc9dc86ee05e4057d6b714cf
Yure Samarone <yuresamarone34@gmail.com>
2022-03-13 00:25:43 -0300

    add: variaveis

--- src/components/CustomBlocklyWorpace/index.tsx
+++ src/components/CustomBlocklyWorpace/index.tsx
@@ -18,30 +18,86 @@ export interface BlocklyWorkpaceProps {
 export const CustomBlocklyWorkpace = ({ javascriptCode, onCodeChange: setJavascriptCode }: BlocklyWorkpaceProps) => {
 
   const [xml, setXml] = useState("");
-  const [list,setList] = useState<Blockly.VariableModel[]>([]);
+  const [list, setList] = useState<Blockly.VariableModel[]>([]);
+  const [currentToolBox, setCurrentToolBox] = useState(toolboxCategories(true, list));
   function workspaceDidChange(workspace: WorkspaceSvg) {
     const code = JavaScript.workspaceToCode(workspace as Workspace);
-   
-    workspace.registerButtonCallback("create_variable", () => {
-      
-      Blockly.Variables.createVariableButtonHandler(workspace as Workspace, undefined, 'create_variable');
-      console.log(workspace.getAllVariableNames());
-      console.log(workspace.getAllVariables());
-      setList(workspace.getAllVariables());
-      console.log(list[0].getId());
-      
-    })
+    if (!workspace.getButtonCallback('create_variable')) {
+      workspace.registerButtonCallback("create_variable", () => {
+        Blockly.Variables.createVariableButtonHandler(workspace as Workspace, undefined, 'create_variable');
+        let newVariables = workspace.getAllVariables();
+        let tmpToolBox: typeof currentToolBox = { kind: '', contents: [] };
+        Object.assign(tmpToolBox, currentToolBox);
+        if (currentToolBox.contents[5].contents.length <= 1) {
+          tmpToolBox.contents[5].contents = [
+            ...currentToolBox.contents[5].contents,
+            {
+              kind: "block",
+              "type": "variables_set",
+              "fields": {
+                "VAR": {
+                  "id": newVariables[newVariables.length - 1].getId(),
+                }
+              }
+            },
+            {
+              kind: "block",
+              "type": "math_change",
+              "fields": {
+                "VAR": {
+                  "id": newVariables[newVariables.length - 1].getId()
+                }
+              },
+              "inputs": {
+                "DELTA": {
+                  "shadow": {
+                    "type": "math_number",
+                    "fields": {
+                      "NUM": 1
+                    }
+                  }
+                }
+              }
+            },
+            {
+              kind: "block",
+              "type": "variables_get",
+              "fields": {
+                "VAR": {
+                  "id": newVariables[newVariables.length - 1].getId()
+                }
+              }
+            }
+          ]
+        } else {
+          tmpToolBox.contents[5].contents = [
+            ...currentToolBox.contents[5].contents,
+            {
+              kind: "block",
+              "type": "variables_get",
+              "fields": {
+                "VAR": {
+                  "id": newVariables[newVariables.length - 1].getId()
+                }
+              }
+            }
+          ]
+        }
+        setCurrentToolBox(tmpToolBox);
+      })
+    }
 
     if (code !== javascriptCode) {
       setJavascriptCode(code);
       console.log(code)
     }
-    
-  }
 
+
+  }
+  console.log(currentToolBox)
   return (
     <BlocklyWorkspace
-      toolboxConfiguration={toolboxCategories(list.length == 0,list)}
+      toolboxConfiguration={currentToolBox}
       className="full"
       workspaceConfiguration={{
         grid: {
@@ -51,6 +107,7 @@ export const CustomBlocklyWorkpace = ({ javascriptCode, onCodeChange: setJavascr
           snap: true,
         },
       }}
+
       onWorkspaceChange={workspaceDidChange}
       onXmlChange={setXml}
     />
