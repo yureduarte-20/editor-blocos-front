99a376549cfede363e818fb605917604a74f7087
saviogodinho2002 <saviogmoiagaia.2002@gmail.com>
2022-03-14 09:06:24 -0300

    zoom-controls

--- src/components/CustomBlocklyWorpace/index.tsx
+++ src/components/CustomBlocklyWorpace/index.tsx
@@ -66,10 +66,20 @@ export const CustomBlocklyWorkpace = ({ javascriptCode, onCodeChange: setJavascr
           length: 3,
           colour: "#ccc",
           snap: true,
-        }
+        },
+        zoom:
+         {controls: true,
+          wheel: true,
+          startScale: 1.0,
+          maxScale: 3,
+          minScale: 0.3,
+          scaleSpeed: 1.2,
+          pinch: true}
       }}
+      
       onWorkspaceChange={workspaceDidChange}
       onXmlChange={setXml}
+      
     />
 
   );
